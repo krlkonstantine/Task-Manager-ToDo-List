6abec2e2f0ac414a65a66855f6754a0b92570de7
krlkonstantine <kostea.023@gmail.com>
2022-12-08 10:25:26 +0300

    small changes

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useCallback} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
@@ -26,10 +26,10 @@ type PropsType = {
     changeTaskTitle: (taskId: string, newTitle: string, todolistId: string) => void
 }
 
-export function Todolist(props: PropsType) {
-    const addTask = (title: string) => {
+export const Todolist = React.memo(function (props: PropsType) {
+    const addTask = useCallback((title: string) => {
         props.addTask(title, props.id);
-    }
+    },[])
 
     const removeTodolist = () => {
         props.removeTodolist(props.id);
@@ -93,6 +93,4 @@ export function Todolist(props: PropsType) {
             </Button>
         </div>
     </div>
-}
-
-
+})
