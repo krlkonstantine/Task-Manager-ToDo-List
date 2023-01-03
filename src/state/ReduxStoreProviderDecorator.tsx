27362b213950aacc2d5f240b5a37cdde56234f8a
krlkonstantine <kostea.023@gmail.com>
2023-01-03 13:34:39 +0300

    decorator implemented

---
+++ src/state/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {Provider} from "react-redux";
+import {AppRootStateType, store} from "./store";
+import {combineReducers, legacy_createStore} from "redux";
+import {tasksReducer} from "./tasks-reducer";
+import {todolistsReducer} from "./todolists-reducer";
+import {v1} from "uuid";
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: 'todolistId1', title: 'What to learn', filter: 'all'},
+        {id: 'todolistId2', title: 'What to buy', filter: 'all'}
+    ],
+    tasks: {
+        ['todolistId1']: [
+            {id: v1(), title: 'HTML&CSS', isDone: false},
+            {id: v1(), title: 'JS', isDone: true}
+        ],
+        ['todolistId2']: [
+            {id: v1(), title: 'Milk', isDone: false},
+            {id: v1(), title: 'React Book', isDone: true}
+        ]
+    }
+}
+export const storybookStore = legacy_createStore(rootReducer,initialGlobalState as AppRootStateType)
+export const ReduxStoreProviderDecorator = (StoryFn: ()=> JSX.Element) => {
+        return <Provider store={storybookStore}>{StoryFn()}</Provider>
+}
--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -4,25 +4,24 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button } from './Button';
 import {AddItemForm} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
+import AppWithRedux from "../AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "../state/ReduxStoreProviderDecorator";
+
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Todolists/AddItemForm',
-  component: AddItemForm,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    onClick: {
-      description:"Button clicked"
-    },
-  },
-} as ComponentMeta<typeof Button>;
+  title: 'Todolists/AppWithredux',
+  component: AppWithRedux,
+  decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof AppWithRedux>;
+
+
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>
 
-export const AddItemFormStory = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-AddItemFormStory.args = {
-  addItem: action("Button clicked")
-};
+export const AppWithReduxStory = Template.bind({});
+
 
