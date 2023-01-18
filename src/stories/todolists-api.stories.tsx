2b596b326a0bf2383e06dfa73a2a4aec16c28c30
krlkonstantine <kostea.023@gmail.com>
2023-01-18 10:17:45 +0300

    some api requests added

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,14 +1,24 @@
-
-
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
 }
+const settings = {
+    withCredentials: true,
+    headers: {
+        "API-KEY": "53f41b80-8e6a-42fb-a09a-fbfdb0f7c77a"
+    }
+}
+
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+            .then((res) => {
+                setState(res.data)
+            })
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
 
@@ -18,6 +28,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let title = 'React&Redux'
+        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title}, settings)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -25,6 +40,11 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let todoId = "601794f7-88ac-4b81-a02f-1586be566bcb"
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/ + ${todoId}`, settings)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
