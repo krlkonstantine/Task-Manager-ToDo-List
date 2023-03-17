59e93844058f5751ec19a77da42e731efcf0f202
krlkonstantine <kostea.023@gmail.com>
2023-03-17 10:48:44 +0300

    errors handling added

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,7 +1,7 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType,
+    status: 'idle' as RequestStatusType,
     error: null as null | string
 }
 
@@ -11,9 +11,17 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
-type ActionsType = any
+
+export const setLoadingStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setErrorAC = (error:null | string) => ({type: 'APP/SET-ERROR', error} as const)
+
+export type SetLoadingStatusACType = ReturnType<typeof setLoadingStatusAC>
+export type SetErrorACType = ReturnType<typeof setErrorAC>
+type ActionsType = SetLoadingStatusACType | SetErrorACType
