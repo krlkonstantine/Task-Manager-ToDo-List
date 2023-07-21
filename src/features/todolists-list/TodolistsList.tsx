46721b02d524da0911ecbd7632519badfdb6cbcd
krlkonstantine <kostea.023@gmail.com>
2023-07-21 10:15:23 +0300

    refactor: removeTask, renameTask and changeTaskStatus handlers were moved into task component

--- src/features/todolists-list/TodolistsList.tsx
+++ src/features/todolists-list/TodolistsList.tsx
@@ -38,14 +38,6 @@ export const TodolistsList = () => {
     addTaskThunk({ title, todolistId });
   }, []);
 
-  const changeStatus = useCallback(function (taskId: string, status: TaskStatuses, todolistId: string) {
-    updateTask({ taskId, domainModel: { status }, todolistId });
-  }, []);
-
-  const changeTaskTitle = useCallback(function (taskId: string, title: string, todolistId: string) {
-    updateTask({ taskId, domainModel: { title }, todolistId });
-  }, []);
-
   const changeFilter = useCallback(function (filter: FilterValuesType, id: string) {
     changeTodolistFilter({ id, filter });
   }, []);
@@ -83,9 +75,7 @@ export const TodolistsList = () => {
                   tasks={allTodolistTasks}
                   changeFilter={changeFilter}
                   addTask={addTask}
-                  changeTaskStatus={changeStatus}
                   removeTodolist={removeTodolist}
-                  changeTaskTitle={changeTaskTitle}
                   changeTodolistTitle={changeTodolistTitle}
                 />
               </Paper>
