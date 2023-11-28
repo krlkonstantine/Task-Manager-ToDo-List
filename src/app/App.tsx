ca189e8277a66eb956e29115da5674919028cb70
krlkonstantine <krlkonstantine@gmail.com>
2023-11-28 14:41:13 +0300

    structure changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,44 +1,71 @@
-import React, { useCallback, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import {CircularProgress} from "@mui/material";
-import "./App.css";
-import { ErrorSnackbar } from "common/components";
-import { useActions } from "common/hooks";
-import { selectIsLoggedIn } from "features/auth/auth.selectors";
-import { selectIsInitialized } from "app/app.selectors";
-import { authThunks } from "features/auth/auth.slice";
-import {AppBarComponent} from "app/app-bar/app-bar";
-import {ContainerForApp} from "app/container/container";
+import React, {useCallback, useEffect} from 'react'
+import './App.css'
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    IconButton,
+    LinearProgress,
+    Toolbar,
+    Typography
+} from '@material-ui/core'
+import {Menu} from '@material-ui/icons'
+import {TodolistsList} from '../features/TodolistsList'
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {useDispatch, useSelector} from 'react-redux'
+import {appActions} from '../features/Application'
+import {Route} from 'react-router-dom'
+import {authActions, Login} from '../features/Auth'
+import {selectIsInitialized, selectStatus} from '../features/Application/selectors'
+import {authSelectors} from '../features/Auth'
+import {useActions} from '../utils/redux-utils'
 
-function App() {
-  const isInitialized = useSelector(selectIsInitialized);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+type PropsType = {}
 
-  const { initializeApp } = useActions(authThunks);
+function App(props: PropsType) {
+    const status = useSelector(selectStatus)
+    const isInitialized = useSelector(selectIsInitialized)
+    const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn)
 
-  useEffect(() => {
-    initializeApp();
-  }, []);
+    const {logout} = useActions(authActions)
+    const {initializeApp} = useActions(appActions)
+    useEffect(() => {
+        if (!isInitialized) {
+            initializeApp()
+        }
+    }, []);
 
-
-  if (!isInitialized) {
+    const logoutHandler = useCallback(() => {
+        logout()
+    }, [])
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
     return (
-      <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
-        <CircularProgress />
-      </div>
-    );
-  }
-
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <ErrorSnackbar />
-        <AppBarComponent isLoggedIn={isLoggedIn}/>
-        <ContainerForApp />
-      </div>
-    </BrowserRouter>
-  );
+            <div className="App">
+                <ErrorSnackbar/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Menu/>
+                        </IconButton>
+                        <Typography variant="h6">
+                            News
+                        </Typography>
+                        {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
+                    </Toolbar>
+                    {status === 'loading' && <LinearProgress/>}
+                </AppBar>
+                <Container fixed>
+                    <Route exact path={'/'} render={() => <TodolistsList demo={false}/>}/>
+                    <Route path={'/login'} render={() => <Login/>}/>
+                </Container>
+            </div>
+    )
 }
 
-export default App;
+export default App
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,20 @@
-import { tasksReducer } from "features/todolists-list/tasks/model/tasks.reducer";
-import { todolistsReducer } from "features/todolists-list/todolists/model/todolists.reducer";
-import { AnyAction, combineReducers } from "redux";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
-import { appReducer } from "app/app.reducer";
-import { authSlice } from "features/auth/auth.slice";
-import { configureStore } from "@reduxjs/toolkit";
+import thunkMiddleware from 'redux-thunk'
+import {configureStore} from '@reduxjs/toolkit'
+import {rootReducer} from './reducers'
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authSlice,
-});
+
 
 export const store = configureStore({
-  reducer: rootReducer,
-});
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
 
-export type AppRootStateType = ReturnType<typeof rootReducer>;
-
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
-
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store;
+window.store = store
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./reducers', () => {
+        store.replaceReducer(rootReducer)
+    })
+}
