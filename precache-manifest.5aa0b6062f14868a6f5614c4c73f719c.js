a4a80692310b34528af6114cc88e27e464b04012
krlkonstantine <krlkonstantine@gmail.com>
2023-11-29 10:46:30 +0300

    Updates

---
+++ precache-manifest.5aa0b6062f14868a6f5614c4c73f719c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217b250c07cdae55a4dd426526265e38",
+    "url": "/Task-Manager-ToDo-List/index.html"
+  },
+  {
+    "revision": "a600148fd2854e3dcf13",
+    "url": "/Task-Manager-ToDo-List/static/css/main.674b6137.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/Task-Manager-ToDo-List/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Task-Manager-ToDo-List/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a600148fd2854e3dcf13",
+    "url": "/Task-Manager-ToDo-List/static/js/main.430fbe6a.chunk.js"
+  },
+  {
+    "revision": "03ffc664ceffa07cd665",
+    "url": "/Task-Manager-ToDo-List/static/js/runtime-main.8c7568c2.js"
+  }
+]);
